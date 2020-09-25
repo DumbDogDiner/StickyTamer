7136a45adb9b2574e5f94df5116175480074fa58
kokumaji <kokumaji@outlook.com>
2020-09-25 18:43:44 +0200

    Update build.gradle & README

--- README.md
+++ README.md
@@ -2,5 +2,24 @@
 
 ### **This project is still WIP!**
 
-Tamer is a [`PaperMC`](https://github.com/papermc/paper) plugin, which not only allows players to 
+Tamer is a plugin for [`PaperMC`](https://github.com/papermc/paper) (1.14 - 1.16.3), which not only allows players to 
 protect their entities from other players, but also enhances the vanilla entity taming/breeding mechanics!
+
+# Features
+* **Lightweight & fast** entity claiming plugin
+* **Easy & innovative** entity taming/breeding mechanics
+* **Absolutely no** dependencies  
+* **Optional** PlaceholderAPI support
+
+
+# Installation 
+
+Due to this plugin depending on private APIs, it is currently not possible to compile it from source. This might change in the future.
+
+**There are no publicly available releases at the moment!**
+
+# Planned Features
+
+* **Add support** for 1.8 - 1.13
+* **Custom** entity attributes & 'spells'
+
--- src/main/java/me/kokumaji/Tamer/Tamer.java
+++ src/main/java/me/kokumaji/Tamer/Tamer.java
@@ -2,8 +2,8 @@ package me.kokumaji.Tamer;
 
 import me.kokumaji.HibiscusAPI.api.translation.Translator;
 import me.kokumaji.Tamer.Commands.MainCommand;
+
 import org.bukkit.Bukkit;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,7 +12,6 @@ import me.kokumaji.Tamer.Listeners.EntityInteractListener;
 import me.kokumaji.Tamer.Objects.GUIs.GUIHandler;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public final class Tamer extends JavaPlugin {
 
@@ -30,15 +29,15 @@ public final class Tamer extends JavaPlugin {
             translate = new Translator(this, config, false);
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }   
 
-        Objects.requireNonNull(getCommand("Tamer")).setExecutor((CommandExecutor) new MainCommand());
+        getCommand("Tamer").setExecutor(new MainCommand());
         GUIHandler.RegisterGUIs();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        
     }
 
     public static Plugin GetPlugin() {
